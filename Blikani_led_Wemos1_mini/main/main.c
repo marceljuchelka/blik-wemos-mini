648f407b44f40e61f1831c754f39ff9167bbefed
marcel <marcel@strihovy>
2021-12-19 12:01:53 +0100

    start

--- Blikani_led_Wemos1_mini/main/main.c
+++ Blikani_led_Wemos1_mini/main/main.c
@@ -23,15 +23,15 @@
 
 
 #define led_pin_mb		GPIO_Pin_2
-#define led_pin_red		GPIO_Pin_15
-#define led_pin_green	GPIO_Pin_12
-#define led_pin_blue	GPIO_Pin_13
+#define led_pin_red		GPIO_Pin_0
+#define led_pin_yellow	GPIO_Pin_12
+#define led_pin_green	GPIO_Pin_14
 
 
 #define led_mb		GPIO_NUM_2
-#define led_red		GPIO_NUM_15
-#define led_green	GPIO_NUM_12
-#define led_blue	GPIO_NUM_13
+#define led_red		GPIO_NUM_0
+#define led_yellow	GPIO_Pin_0
+#define led_green	led_pin_green
 
 gpio_config_t	gpio_cfg;
 adc_config_t	adc_cfg;
@@ -42,7 +42,7 @@ void app_main()
 {
 
 	uint8_t uroven = 0;
-	gpio_cfg.pin_bit_mask = led_pin_mb | led_pin_red | led_pin_green | led_pin_blue;
+	gpio_cfg.pin_bit_mask = GPIO_Pin_2 | GPIO_Pin_0 | GPIO_Pin_12 | GPIO_Pin_14;
 	gpio_cfg.mode = GPIO_MODE_DEF_OUTPUT;
 	gpio_config(&gpio_cfg);
 	adc_cfg.mode = ADC_READ_TOUT_MODE;
@@ -51,10 +51,10 @@ void app_main()
 	while(1){
 		uroven^= 1;
 		gpio_set_level(led_green, uroven);
-		vTaskDelay(100/portTICK_PERIOD_MS);
+		vTaskDelay(300/portTICK_PERIOD_MS);
 		gpio_set_level(led_red, uroven);
 		vTaskDelay(200/portTICK_PERIOD_MS);
-		gpio_set_level(led_blue, uroven);
+		gpio_set_level(led_pin_yellow, uroven);
 		vTaskDelay(300/portTICK_PERIOD_MS);
 		gpio_set_level(led_mb, uroven);
 		vTaskDelay(200/portTICK_PERIOD_MS);
